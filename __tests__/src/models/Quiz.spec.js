294a2ad2b6232370fe93d9c17c34f42dc54458f7
shodayuki <yshoda1227@gmail.com>
2020-03-31 16:46:03 +0900

    Added Quiz.js and Quiz.spec.js

---
+++ __tests__/src/models/Quiz.spec.js
@@ -0,0 +1,83 @@
+import Quiz from '../../../src/models/Quiz';
+
+const createMockQuiz = () => {
+  return {
+    question: 'クイズの問題',
+    correctAnswer: '答え',
+    incorrectAnswers: [
+      '不正解1',
+      '不正解2',
+      '不正解3'
+    ],
+  }
+};
+
+describe('Quizクラスのテスト', () => {
+  it('importチェック', () => {
+    expect( typeof Quiz ).toStrictEqual('function');
+  });
+
+  describe('インスタンスメソッド', () => {
+    describe('constructor', () => {
+      it('コンストラクタで渡した値をプロパティに保持する', () => {
+        const quizData = createMockQuiz();
+        const quiz = new Quiz(quizData);
+
+        expect( quiz._question ).toStrictEqual(quizData.question);
+        expect( quiz._correctAnswer ).toStrictEqual(quizData.correctAnswer);
+        expect( quiz._incorrectAnswer ).toStrictEqual(quizData.incorrectAnswers);
+      });
+    });
+  });
+
+  describe('getter', () => {
+    it('questionとcorrectAnswerのgetterが増える', () => {
+      const quizData = createMockQuiz();
+      const quiz = new Quiz(quizData);
+
+      expect( quiz.question ).toStrictEqual(quizData.question);
+      expect( quiz.correctAnswer ).toStrictEqual(quizData.correctAnswer);
+      expect( quiz.incorrectAnswers ).toStrictEqual(undefined);
+    });
+  });
+
+  describe('shuffleメソッド', () => {
+    it('シャッフルされる', () => {
+      const quizData = createMockQuiz();
+      const quiz = new Quiz(quizData);
+
+      const shuffledAnswers1 = quiz.shuffleAnswers();
+      const shuffledAnswers2 = quiz.shuffleAnswers();
+      expect(shuffledAnswers1).not.toStrictEqual(shuffledAnswers2);
+    });
+  });
+
+  describe('judgeCorrectAnswerメソッド', () => {
+    it('引数の値が正解ならtrue,不正解ならfalseが通る', () => {
+      const quizData = createMockQuiz();
+      const quiz = new Quiz(quizData);
+
+      expect(quiz.judgeCorrectAnswer(quizData.correctAnswer))
+        .toStrictEqual(true);
+
+      quizData.incorrectAnswers.forEach(incorrectAnswer => {
+        expect(quiz.judgeCorrectAnswer(incorrectAnswer))
+          .toStrictEqual(false);
+      });
+    });
+  });
+
+  describe('クラスメソッド', () => {
+    describe('fetchAndCreateQuizzesメソッド', () => {
+      it('10件のQuizインスタンスが返る', async () => {
+        const quizzes = await Quiz.fetchAndCreateQuizzes();
+
+        expect ( Array.isArray(quizzes) ).toStrictEqual(true);
+        expect (quizzes.length).toStrictEqual(10);
+        quizzes.forEach(quiz => {
+          expect( quiz instanceof Quiz ).toStrictEqual(true);
+        });
+      });
+    });
+  });
+});
---
+++ src/models/Quiz.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import he from 'he';
+import QuizFetcher from '../data_fetchers/QuizFetcher';
+
+class Quiz {
+  constructor({question, correctAnswer, incorrectAnswers}){
+    console.log(question, correctAnswer, incorrectAnswers, '@@@');
+
+    this._question = question;
+    this._correctAnswer = correctAnswer;
+    this._incorrectAnswers = [...incorrectAnswers];
+  }
+
+  get question() {
+    return this._question;
+  }
+
+  get correctAnswer() {
+    return this._correctAnswer;
+  }
+
+  shuffleAnswers() {
+    return _.shuffle([
+      this._correctAnswer,
+      ...this._incorrectAnswers
+    ]);
+  }
+
+  judgeCorrectAnswer(answer) {
+    return answer === this._correctAnswer;
+  }
+
+  static async fetchAndCreateQuizzes() {
+    const quizDataList = await QuizFetcher.fetch();
+
+    return quizDataList.results.map(result => {
+      return {
+        question: he.decode(result.question),
+        correctAnswer: he.decode(result.correct_answer),
+        incorrectAnswers: result.incorrect_answers.map(str => he.decode(str))
+      };
+    })
+    .map(quizData => {
+      return new Quiz(quizData);
+    })
+  }
+}
+
+export default Quiz;
