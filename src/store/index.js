9ee0f2b02c7d8c8b2e3770bddca660c86d2ac5ef
shodayuki <yshoda1227@gmail.com>
2020-04-01 00:48:25 +0900

    Rerite src/store/index.js and install package.json

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [thunk];
 
 const store = createStore(
